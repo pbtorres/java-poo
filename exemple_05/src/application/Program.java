dd52bc089d29316cd2f3c0dc95fdf7f4680ece34
Pablo Torres Ribeiro <pablo.torres@nextv.tv.br>
2021-09-09 15:32:21 -0300

    Vetores parte 2

---
+++ exemple_05/src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("ENTER THE QUANTITY OF PRODUCTS:");
+		
+		int n = sc.nextInt();
+		Product[] products = new Product[n];
+
+		System.out.println("ENTER NAME AND PRINCE:");
+		
+		for(int i=0; i<products.length; i++) {
+			sc.nextLine();
+			String name = sc.nextLine();
+			double price = sc.nextDouble();
+			products[i] = new Product(name, price);
+		}
+		
+		double sum = 0.0;
+		
+		for (int i=0; i<products.length; i++) {
+			sum += products[i].getPrice();
+		}
+		
+		double avg = sum / n;
+		
+		System.out.printf("AVERAGE PRINCE = %.2f%n", avg );
+		
+		sc.close();
+	}
+
+}
