87d0c30a951b7190199dc778e1b8c26654f90e45
Pablo Torres <pablotorreeifel@gmail.com>
2022-06-24 15:24:22 -0300

    Try-with-resources, FileReader e BufferedReader

--- exemple_22/src/application/Program.java
+++ exemple_22/src/application/Program.java
@@ -13,38 +13,18 @@ public class Program {
 
 	public static void main(String[] args) {
 		String path = "/Users/PabloTorres/Documents/in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){ //Bloco try-with-resources
 			String line = br.readLine();
 			
 			while(line != null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
-		} 
+		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 		
-		finally {
-			try {
-				if (fr != null) {
-					fr.close();
-				}	
-				if (br != null) {
-					br.close();	
-				}	
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
 	}
-
 }
