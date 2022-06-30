2a86bb2bf4bc3910370693cff751d0082532e644
Pablo Torres <pablotorreeifel@gmail.com>
2022-06-30 00:24:40 -0300

    Interfaces, Classes Abstratas, Herança

---
+++ exemple_28/src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import model.entities.AbstractShape;
+import model.entities.Circle;
+import model.entities.Rectagle;
+import model.enums.Color;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		AbstractShape ab1 = new Circle(Color.BLACK, 2.0);
+		AbstractShape ab2 = new Rectagle(Color.WHITE, 3.0, 4.0);
+		
+		System.out.println("Cicle color: " + ab1.getColor());
+		System.out.println("Cicle area: " + String.format("%.3f", ab1.area()));
+		System.out.println("Rectangle color: " + ab2.getColor());
+		System.out.println("Rectangle area: " + String.format("%.3f", ab2.area()));
+		
+	}
+
+}
---
+++ exemple_28/src/model/entities/Rectagle.java
@@ -0,0 +1,37 @@
+package model.entities;
+
+import model.enums.Color;
+
+public class Rectagle extends AbstractShape {
+
+	private double width;
+	private double height;
+	
+	public Rectagle(Color color, double width, double height) {
+		super(color);
+		this.width = width;
+		this.height = height;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	@Override
+	public double area() {
+		return width * height;
+	}
+
+}
