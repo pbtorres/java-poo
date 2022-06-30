458d1c2400b413f9e2a17fc7b587f9723ccc3c10
Pablo Torres <pablotorreeifel@gmail.com>
2022-06-30 00:14:00 -0300

    Interfaces

---
+++ exemple_27/src/model/entities/Contract.java
@@ -0,0 +1,61 @@
+package model.entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Contract {
+	
+	private int number;
+	private Date date;
+	private Double totalValue;
+	
+	private List<Installment> installments = new ArrayList<>();
+	
+	public Contract() { 
+		
+	}
+
+	public Contract(int number, Date date, Double totalValue) {
+		this.number = number;
+		this.date = date;
+		this.totalValue = totalValue;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(Double totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	public List<Installment> getInstallment() {
+		return installments;
+	}
+
+	public void addInstallment(Installment installment) {
+		installments.add(installment);
+	}
+	
+	public void removeInstallment(Installment installment) {
+		installments.remove(installment);
+	}
+	
+}
