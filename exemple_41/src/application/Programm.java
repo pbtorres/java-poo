13f975d4807dd9a82661a354de8e0c757a08fac6
Pablo Torres <pablotorreeifel@gmail.com>
2022-07-05 18:29:43 -0300

    Usando classe Anônina

--- exemple_41/src/application/Programm.java
+++ exemple_41/src/application/Programm.java
@@ -1,12 +1,12 @@
 /*
- * Using interface Comparator
+ * Usando Classe anônima
  */
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-import entities.MyComparator;
 import entities.Product;
 
 public class Programm {
@@ -17,7 +17,14 @@ public class Programm {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		list.sort(new MyComparator());
+		Comparator<Product> comp = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toLowerCase().compareTo(p2.getName().toLowerCase());
+			}
+		};
+		
+		list.sort(comp);
 		
 		for(Product p : list) {
 			System.out.println(p);
