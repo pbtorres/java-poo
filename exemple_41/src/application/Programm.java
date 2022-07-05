f99c59e3e34f4ed176070948edc8d7f1285c5fce
Pablo Torres <pablotorreeifel@gmail.com>
2022-07-05 18:47:12 -0300

    Passnando expressão lambda como argumento

--- exemple_41/src/application/Programm.java
+++ exemple_41/src/application/Programm.java
@@ -1,10 +1,9 @@
 /*
- * Usando expressão lambda
+ * Usando expressão lambda como argumento
  */
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -17,11 +16,7 @@ public class Programm {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		Comparator<Product> comp = (p1, p2) -> {
-			return p1.getName().toLowerCase().compareTo(p2.getName().toLowerCase());
-		};
-		
-		list.sort(comp);
+		list.sort((p1, p2) -> p1.getName().toLowerCase().compareTo(p2.getName().toLowerCase()));
 		
 		for(Product p : list) {
 			System.out.println(p);
