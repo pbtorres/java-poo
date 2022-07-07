e26b466b08cd6c288bc9babccb82662728536ffe
Pablo Torres <pablotorreeifel@gmail.com>
2022-07-06 23:46:37 -0300

    Usando interface Comparator<T>

---
+++ exemple_42/src/application/Program.java
@@ -0,0 +1,27 @@
+/*
+ * Usando interface Comparator<T>
+ */
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import util.PriceUpdate;
+
+public class Program {
+
+	public static void main(String[] args) {
+List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		list.forEach(new PriceUpdate());
+		
+		list.forEach(System.out::println);
+		
+	}
+
+}
