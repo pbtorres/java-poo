e9deb6008c459b195c7e0b2c08278c1a58c01f05
Pablo Torres <pablotorreeifel@gmail.com>
2022-07-08 01:07:15 -0300

    Usando expressão lambda inline

--- exemple_43/src/application/Program.java
+++ exemple_43/src/application/Program.java
@@ -1,11 +1,10 @@
 /*
- * Usando expressão lambda declarada
+ * Usando expressão lambda inline
  */
 package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -19,9 +18,7 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		Function<Product, String> func = p -> p.getName().toUpperCase();
-		
-		List<String> names = list.stream().map(func).collect(Collectors.toList());
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 		
