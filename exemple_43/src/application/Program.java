fd798be0837abe7ae1655fa95d134bd24e0c07de
Pablo Torres <pablotorreeifel@gmail.com>
2022-07-08 01:06:14 -0300

    Usando expressão lambda declarada

--- exemple_43/src/application/Program.java
+++ exemple_43/src/application/Program.java
@@ -1,10 +1,11 @@
 /*
- * Usando retefence method com método não estático
+ * Usando expressão lambda declarada
  */
 package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -18,7 +19,9 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 		
