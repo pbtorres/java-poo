aad3e43db66d9d45a34aa6fe94f75a6963861547
Pablo Torres <pablotorreeifel@gmail.com>
2022-07-08 00:55:40 -0300

    Usando interface Function<T, R>

---
+++ exemple_43/src/application/Program.java
@@ -0,0 +1,27 @@
+/*
+ * Usando a interface Function<T, R>
+ */
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import entities.Product;
+import util.UpperCaseName;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		
+		names.forEach(System.out::println);
+		
+	}
+}
